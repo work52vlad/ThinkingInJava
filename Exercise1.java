bdf031cffddc94b86d7771bc52cf339bb64d3854
work52vlad <work52vlad@gmail.com>
2020-07-20 16:15:58 +0300

    add Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,12 @@
+public class Exercise1 {
+
+    static int int_whyImNotInitialized;
+    static char char_whyImNotInitialized;
+    public static void main(String[] args) {
+
+        System.out.println(int_whyImNotInitialized);
+        System.out.println(char_whyImNotInitialized);
+
+    }
+
+}
