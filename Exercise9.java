e7937d1052518590dfabcfa39595d22c776efcb2
work52vlad <work52vlad@gmail.com>
2020-07-20 17:10:26 +0300

    add Exercise9.java

---
+++ Exercise9.java
@@ -0,0 +1,13 @@
+public class Exercise9 {
+
+    public static void main(String[] args) {
+
+        Integer int_autoboxing = 1;
+        String string_autoboxing = "sdf";
+        Boolean char_autoboxing = false;
+
+
+
+    }
+
+}
