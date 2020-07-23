40ac3fe9a9e7c9862c58277de57d2fcfa4ef51d4
work52vlad <work52vlad@gmail.com>
2020-07-23 21:29:30 +0300

    add Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,16 @@
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        Useless useless = new Useless();
+        System.out.println(useless.im_null);
+
+    }
+
+}
+
+class Useless {
+
+    String im_null;
+
+}
